57c1db5c65fa139e06167f773a416fc04339250b
Michael Greene <greeneink4@gmail.com>
2017-10-11 21:35:26 -0600

    Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Senators from './components/Senators';
 
 class App extends Component {
   render() {
     return (
+    <MuiThemeProvider>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">The Elephants and Jackasses</h1>
+          <h1 className="App-title">Elephants and Jackasses</h1>
         </header>
         <div>
           <Senators />
           </div>
       </div>
+    </MuiThemeProvider>
     );
   }
 }
